c70041ce69b7369cc7f789e48701044a57bcfcfb
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-12-05 11:18:27 +0530

    creating csrf token for reseting pd

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const crypto = require("crypto");
+
 const bcrpyt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
@@ -129,5 +131,40 @@ exports.getReset = (req, res, next) => {
 		path: "/logresetin",
 		pageTitle: "Reset Password",
 		errorMessage: message,
+	});
+};
+
+exports.postReset = (req, res, next) => {
+	crypto.randomBytes(32, (err, buffer) => {
+		if (err) {
+			console.log(err);
+			return res.redirect("/reset");
+		}
+		const token = buffer.toString("hex");
+		User.findOne({ email: req.body.email })
+			.then((user) => {
+				if (!user) {
+					req.flash("error", "No account with that email found.");
+					return res.redirect("/reset");
+				}
+				user.resetToken = token;
+				user.resetTokenExpiration = Date.now() + 3600000;
+				return user.save();
+			})
+			.then((result) => {
+				res.redirect("/");
+				transporter.sendMail({
+					to: req.body.email,
+					from: "thomasshelby1709@gmail.com",
+					subject: "Password reset",
+					html: `
+			  <p>You requested a password reset</p>
+			  <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+			`,
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	});
 };
