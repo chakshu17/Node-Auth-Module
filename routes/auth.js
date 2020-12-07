3309e7a8f798bb82119235c956b0bdacb938bd7a
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-12-07 11:57:16 +0530

    Sanitize the input data

--- routes/auth.js
+++ routes/auth.js
@@ -14,11 +14,12 @@ router.post(
 	[
 		body("email")
 			.isEmail()
-			.withMessage("Please enter a valid email address."),
+			.withMessage("Please enter a valid email address.")
+			.normalizeEmail(),
 		body("password", "Password has to be valid.")
 			.isLength({ min: 5 })
-			.isAlphanumeric(),
-		
+			.isAlphanumeric()
+			.trim(),
 	],
 	authController.postLogin
 );
@@ -45,19 +46,23 @@ router.post(
 						);
 					}
 				});
-			}),
+			})
+			.normalizeEmail(),
 		body(
 			"password",
 			"Please enter a password with number and text only with min of 6 characters "
 		)
 			.isLength({ min: 8 })
-			.isAlphanumeric(),
-		body("confirmPassword").custom((value, { req }) => {
-			if (value !== req.body.password) {
-				throw new Error("Passwords have to matched");
-			}
-			return true;
-		}),
+			.isAlphanumeric()
+			.trim(),
+		body("confirmPassword")
+			.trim()
+			.custom((value, { req }) => {
+				if (value !== req.body.password) {
+					throw new Error("Passwords have to matched");
+				}
+				return true;
+			}),
 	],
 	authController.postSignup
 );
