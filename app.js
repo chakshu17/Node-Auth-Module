8b4b4aba5ed132d033e2def0388e161a3b5e6c82
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-12-08 10:58:12 +0530

    Update

--- app.js
+++ app.js
@@ -46,6 +46,12 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+	res.locals.isAuthenticatedf = req.session.isLoggedIn;
+	res.locals.csrfToken = req.csrfToken();
+	next();
+});
+
+app.use((req, res, next) => {
 	if (req.session.user) {
 		User.findById(req.session.user)
 			.then((user) => {
@@ -56,17 +62,11 @@ app.use((req, res, next) => {
 				next();
 			})
 			.catch((err) => {
-				throw new Error(err);
+				next(new Error(err));
 			});
 	} else {
 		next();
 	}
-});
-
-app.use((req, res, next) => {
-	res.locals.isAuthenticated = req.session.isLoggedIn;
-	res.locals.csrfToken = req.csrfToken();
-	next();
 });
 
 app.use("/admin", adminRoutes);
@@ -78,7 +78,11 @@ app.get("/500", errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-	res.redirect("/500");
+	res.status(500).render("500", {
+		pageTitle: "Error!",
+		path: "/404",
+		isAuthenticated: req.session.isLoggedIn,
+	});
 });
 
 mongoose
