4d8fe5075f7e35460eb8db1b31d49bbef3fd0610
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-12-10 22:13:53 +0530

    Update

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const crypto = require("crypto");
 
 const bcrpyt = require("bcryptjs");
+
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
 const { validationResult } = require("express-validator");
