ba49824ec1fb5f8f4c558eb16b7f9c025326ee2e
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-12-22 15:18:39 +0530

    File Type Headers

--- controllers/shop.js
+++ controllers/shop.js
@@ -166,12 +166,14 @@ exports.getCheckout = (req, res, next) => {
 exports.getInvoice = (req, res, next) => {
 	const orderId = req.params.orderId;
 	const invoiceName = "invoice-" + orderId + ".pdf";
-	const invoicePath = path.join("data", "invoice", invoiceName);
+	const invoicePath = path.join("data", "invoices", invoiceName);
 
 	fs.readFile(invoicePath, (err, data) => {
 		if (err) {
 			return next(err);
 		}
+		res.setHeader("Content-Type", "applicate/pdf");
+		res.setHeader("Content-Disposition", "inline;filename=" + invoiceName + "");
 		res.send(data);
 	});
 };
