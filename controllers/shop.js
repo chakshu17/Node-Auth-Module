27fc357fbfa1436dc98c7146e65bb530b14bcbdf
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-12-24 12:43:34 +0530

    Dynamic Pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,7 +6,7 @@ const Order = require("../models/order");
 
 const PDFDocument = require("pdfkit");
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 // to get all products
 exports.getProducts = (req, res, next) => {
@@ -45,16 +45,28 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-	const page = req.query.page;
-
-	Product.find() // gives all items
-		.skip((page - 1) * ITEMS_PER_PAGE) // to skip first x amts of results,
-		.limit(ITEMS_PER_PAGE) // to limit the amt of data we fetch to no. we specify here
+	const page = +req.query.page || 1;
+	// skip() to skip first x amts of results,
+	//  limit() to limit the amt of data we fetch to no. we specify here
+	Product.find()
+		.countDocuments()
+		.then((numProducts) => {
+			totalItems = numProducts;
+			return Product.find()
+				.skip((page - 1) * ITEMS_PER_PAGE)
+				.limit(ITEMS_PER_PAGE);
+		})
 		.then((products) => {
 			res.render("shop/index", {
 				prods: products,
 				pageTitle: "Shop",
 				path: "/",
+				currentPage: page,
+				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+				hasPreviousPage: page > 1,
+				nextPage: page + 1,
+				previousPage: page - 1,
+				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
 			});
 		})
 		.catch((err) => {
