2827750c46fee0b38c640418396d02ee7c511e3b
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-12-27 21:04:00 +0530

    Update

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 const path = require("path");
+
 const express = require("express");
 const adminController = require("../controllers/admin");
+
 const isAuth = require("../middleware/is-auth");
 const { body } = require("express-validator");
 
